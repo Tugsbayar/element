024d994c99a5d6b1996378717c498e19e1904b24
Tugsbayar B <tgs19g@gmail.com>
2017-09-29 13:43:22 +0900

    mn.js
    Language file for Mongolian.

---
+++ src/locale/lang/mn.js
@@ -0,0 +1,104 @@
+export default {
+  el: {
+    colorpicker: {
+      confirm: 'Зөвшөөрөх',
+      clear: 'Цэвэрлэх'
+    },
+    datepicker: {
+      now: 'Одоо',
+      today: 'Өнөөдөр',
+      cancel: 'Цуцлах',
+      clear: 'Цэвэрлэх',
+      confirm: 'зөвшөөрөх',
+      selectDate: 'Огноо сонгох',
+      selectTime: 'Цаг сонгох',
+      startDate: 'Эхлэх огноо',
+      startTime: 'Эхлэх цаг',
+      endDate: 'Дуусах огноо',
+      endTime: 'Дуусах цаг',
+      year: '',
+      month1: 'I сар',
+      month2: 'II сар',
+      month3: 'III сар',
+      month4: 'IV сар',
+      month5: 'V сар',
+      month6: 'VI сар',
+      month7: 'VII сар',
+      month8: 'VIII сар',
+      month9: 'IX сар',
+      month10: 'X сар',
+      month11: 'XI сар',
+      month12: 'XII сар',
+      // week: 'week',
+      weeks: {
+        sun: 'Ням',
+        mon: 'Дав',
+        tue: 'Мяг',
+        wed: 'Лха',
+        thu: 'Бүр',
+        fri: 'Баа',
+        sat: 'Бям'
+      },
+      months: {
+        jan: 'I сар',
+        feb: 'II сар',
+        mar: 'III сар',
+        apr: 'IV сар',
+        may: 'V сар',
+        jun: 'VI сар',
+        jul: 'VII сар',
+        aug: 'VIII сар',
+        sep: 'IX сар',
+        oct: 'X сар',
+        nov: 'XI сар',
+        dec: 'XII сар'
+      }
+    },
+    select: {
+      loading: 'Ачааллаж байна',
+      noMatch: 'Таарах илэрцгүй',
+      noData: 'Мэдээлэл байхгүй',
+      placeholder: 'Сонгох'
+    },
+    cascader: {
+      noMatch: 'Таарах илэрцгүй',
+      loading: 'Ачааллаж байна',
+      placeholder: 'Сонгох'
+    },
+    pagination: {
+      goto: 'Шилжих(рүү)',
+      pagesize: '/page',
+      total: 'Нийт {total}',
+      pageClassifier: ''
+    },
+    messagebox: {
+      title: 'Зурвас',
+      confirm: 'Зөвшөөрөх',
+      cancel: 'Цуцлах',
+      error: 'Illegal input'
+    },
+    upload: {
+      delete: 'Устгах',
+      preview: 'Харагдах байдал',
+      continue: 'Үргэлжлүүлэх'
+    },
+    table: {
+      emptyText: 'Мэдээлэл байхгүй',
+      confirmFilter: 'Зөвшөөрөх',
+      resetFilter: 'Анхны утга',
+      clearFilter: 'Бүгд',
+      sumText: 'Нийлбэр'
+    },
+    tree: {
+      emptyText: 'Мэдээлэл байхгүй'
+    },
+    transfer: {
+      noMatch: 'Таарах илэрцгүй',
+      noData: 'Мэдээлэл байхгүй',
+      titles: ['Жагсаалт 1', 'Жагсаалт 2'], // to be translated
+      filterPlaceholder: 'Товч дарна уу', // to be translated
+      noCheckedFormat: '{total} бичлэг', // to be translated
+      hasCheckedFormat: '{checked}/{total} шалгасан' // to be translated
+    }
+  }
+};
